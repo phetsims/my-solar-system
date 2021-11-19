8be6df223d3ca01a628ad3beaef31e73f9e8fa2e
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:55 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/mySolarSystemStrings.ts
+++ js/mySolarSystemStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import mySolarSystem from './mySolarSystem.js';
 
-const mySolarSystemStrings = getStringModule( 'MY_SOLAR_SYSTEM' );
+type StringsType = {
+  'my-solar-system': {
+    'title': string
+  }
+};
+
+const mySolarSystemStrings = getStringModule( 'MY_SOLAR_SYSTEM' ) as StringsType;
 
 mySolarSystem.register( 'mySolarSystemStrings', mySolarSystemStrings );
 
