5ef874bd5d8ad73b52aa5882393eafb94aa8ed3f
Sam Reid <reids@colorado.edu>
2022-06-09 14:10:15 -0600

    Remove phet-io features from IReadOnlyProperty and TinyProperty, and use ReadOnlyProperty where appropriate, see https://github.com/phetsims/axon/issues/382

--- js/common/view/MySolarSystemCheckbox.ts
+++ js/common/view/MySolarSystemCheckbox.ts
@@ -10,14 +10,14 @@ import Checkbox, { CheckboxOptions } from '../../../../sun/js/Checkbox.js';
 import mySolarSystem from '../../mySolarSystem.js';
 import MySolarSystemColors from '../MySolarSystemColors.js';
 import optionize from '../../../../phet-core/js/optionize.js';
-import IProperty from '../../../../axon/js/IProperty.js';
 import { Node } from '../../../../scenery/js/imports.js';
+import Property from '../../../../axon/js/Property.js';
 
 export type MySolarSystemCheckboxOptions = CheckboxOptions;
 
 export default class MySolarSystemCheckbox extends Checkbox {
 
-  constructor( content: Node, property: IProperty<boolean>, providedOptions?: MySolarSystemCheckboxOptions ) {
+  constructor( content: Node, property: Property<boolean>, providedOptions?: MySolarSystemCheckboxOptions ) {
 
     const options = optionize<MySolarSystemCheckboxOptions, {}, CheckboxOptions>()( {
       boxWidth: 14,
