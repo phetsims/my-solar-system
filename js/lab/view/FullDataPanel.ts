a6651aefbc226ed4b221938aaf74160fda84806b
AgustinVallejo <agustin.vallejo@udea.edu.co>
2022-09-13 11:15:40 -0500

    Adding Interactive Number Display to full data panel

--- js/lab/view/FullDataPanel.ts
+++ js/lab/view/FullDataPanel.ts
@@ -8,22 +8,25 @@
  */
 
 import mySolarSystem from '../../mySolarSystem.js';
-import { GridBox, GridBoxOptions, Node, Text } from '../../../../scenery/js/imports.js';
+import { FireListener, GridBox, GridBoxOptions, Node, Text } from '../../../../scenery/js/imports.js';
 import RangeWithValue from '../../../../dot/js/RangeWithValue.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import NumberDisplay from '../../../../scenery-phet/js/NumberDisplay.js';
+import KeypadDialog from '../../../../scenery-phet/js/keypad/KeypadDialog.js';
+import NumberDisplay, { NumberDisplayOptions } from '../../../../scenery-phet/js/NumberDisplay.js';
 import ShadedSphereNode from '../../../../scenery-phet/js/ShadedSphereNode.js';
 import MySolarSystemColors from '../../common/MySolarSystemColors.js';
 import CommonModel from '../../common/model/CommonModel.js';
-import { EmptySelfOptions, optionize3 } from '../../../../phet-core/js/optionize.js';
+import { combineOptions, EmptySelfOptions, optionize3 } from '../../../../phet-core/js/optionize.js';
 import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import MySolarSystemConstants from '../../common/MySolarSystemConstants.js';
 import LabModel from '../model/LabModel.js';
-import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import MySolarSystemStrings from '../../MySolarSystemStrings.js';
+import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
+import MappedProperty from '../../../../axon/js/MappedProperty.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import TProperty from '../../../../axon/js/TProperty.js';
 
-//REVIEW: export!
-type FullDataPanelOptions = PanelOptions;
+export type FullDataPanelOptions = PanelOptions;
 
 export default class FullDataPanel extends Panel {
   private readonly dataBox: DataBox;
@@ -52,6 +55,7 @@ class DataBox extends GridBox {
 
   private readonly model: CommonModel;
   private readonly massRange: RangeWithValue;
+  private readonly positionRange: RangeWithValue;
   private tempChildren: Node[];
 
   public constructor( model: CommonModel, providedOptions?: Partial<DataBoxOptions> ) {
@@ -61,11 +65,23 @@ class DataBox extends GridBox {
     } );
     this.model = model;
     this.massRange = new RangeWithValue( 1, 300, 100 );
+    this.positionRange = new RangeWithValue( -400, 400, 0 );
     this.tempChildren = [];
     this.update();
   }
 
   public update(): void {
+
+    // KeypadDialog
+    const keypadDialog = new KeypadDialog( {
+      keypadOptions: {
+        accumulatorOptions: {
+          // {number} - maximum number of digits that can be entered on the keypad.
+          maxDigits: 8
+        }
+      }
+    } );
+
     // Whenever the number of bodies change, repopulate the dataBox
     this.tempChildren = [
       new Text( MySolarSystemStrings.dataPanel.MassStringProperty, { font: new PhetFont( 20 ), layoutOptions: { column: 1, row: 0 } } ),
@@ -81,25 +97,70 @@ class DataBox extends GridBox {
       const color = MySolarSystemColors.bodiesPalette[ i ];
       this.tempChildren.push(
         new ShadedSphereNode( 15, { mainColor: color, layoutOptions: { column: 0, row: i + 2 } } ),
-        new NumberDisplay(
+        new InteractiveNumberDisplay(
           this.model.bodies[ i ].massProperty,
-          this.massRange, { layoutOptions: { column: 1, row: i + 2 } } ),
-        new NumberDisplay(
-          new DerivedProperty( [ this.model.bodies[ i ].positionProperty ], position => position.x ),
-          this.massRange, { layoutOptions: { column: 2, row: i + 2 } } ),
-        new NumberDisplay(
-          new DerivedProperty( [ this.model.bodies[ i ].positionProperty ], position => position.y ),
-          this.massRange, { layoutOptions: { column: 3, row: i + 2 } } ),
-        new NumberDisplay(
-          new DerivedProperty( [ this.model.bodies[ i ].velocityProperty ], velocity => velocity.x ),
-          this.massRange, { layoutOptions: { column: 4, row: i + 2 } } ),
-        new NumberDisplay(
-          new DerivedProperty( [ this.model.bodies[ i ].velocityProperty ], velocity => velocity.y ),
-          this.massRange, { layoutOptions: { column: 5, row: i + 2 } } )
+          this.massRange, keypadDialog, { layoutOptions: { column: 1, row: i + 2 } } ),
+        new InteractiveNumberDisplay(
+          new MappedProperty( this.model.bodies[ i ].positionProperty, {
+            bidirectional: true,
+            map: position => position.x,
+            inverseMap: ( x : number ) => new Vector2( x, this.model.bodies[ i ].positionProperty.value.y )
+          } ),
+          this.positionRange, keypadDialog, { layoutOptions: { column: 2, row: i + 2 } } ),
+        new InteractiveNumberDisplay(
+          new MappedProperty( this.model.bodies[ i ].positionProperty, {
+            bidirectional: true,
+            map: position => position.y,
+            inverseMap: ( y : number ) => new Vector2( this.model.bodies[ i ].positionProperty.value.x, y )
+          } ),
+          this.positionRange, keypadDialog, { layoutOptions: { column: 3, row: i + 2 } } ),
+        new InteractiveNumberDisplay(
+          new MappedProperty( this.model.bodies[ i ].velocityProperty, {
+            bidirectional: true,
+            map: velocity => velocity.x,
+            inverseMap: ( x : number ) => new Vector2( x, this.model.bodies[ i ].velocityProperty.value.y )
+          } ),
+          this.positionRange, keypadDialog, { layoutOptions: { column: 4, row: i + 2 } } ),
+        new InteractiveNumberDisplay(
+          new MappedProperty( this.model.bodies[ i ].velocityProperty, {
+            bidirectional: true,
+            map: velocity => velocity.y,
+            inverseMap: ( y : number ) => new Vector2( this.model.bodies[ i ].velocityProperty.value.x, y )
+          } ),
+          this.positionRange, keypadDialog, { layoutOptions: { column: 5, row: i + 2 } } )
         );
     }
     this.children = this.tempChildren;
   }
+}
+
+class InteractiveNumberDisplay extends NumberDisplay {
+  public constructor(
+    property: TProperty<number>,
+    range: RangeWithValue,
+    keypadDialog: KeypadDialog,
+    providedOptions?: NumberDisplayOptions
+  ) {
+
+    const options = combineOptions<NumberDisplayOptions>( {
+      cursor: 'pointer'
+    }, providedOptions );
+
+    super( property, range, options );
+
+    this.addInputListener( new FireListener( {
+      fire: () => {
+        keypadDialog.beginEdit( value => {
+          property.value = value;
+        }, range, new PatternStringProperty( MySolarSystemStrings.gridStringProperty, {
+          units: 'AU'
+        } ), () => {
+          // no-op
+        } );
+      },
+      fireOnDown: true
+    } ) );
+    }
 }
 
 mySolarSystem.register( 'FullDataPanel', FullDataPanel );
