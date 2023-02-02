b04bbfbf62ab5069dd780351e73adee3ccd8ee5a
phet-dev <phetadmin@gmail.com>
2023-02-02 04:49:16 -0700

    update copyright dates from daily grunt work

--- js/common/view/VectorNode.ts
+++ js/common/view/VectorNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * Draws a vector for a Body, such as a force vector or velocity vector.
