76cb57d132bcb19879ee41bac690c56257d07d98
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-02 09:39:34 -0700

    Review comment

--- js/common/view/InteractiveNumberDisplay.ts
+++ js/common/view/InteractiveNumberDisplay.ts
@@ -50,6 +50,7 @@ export default class InteractiveNumberDisplay extends NumberDisplay {
       useRichTextRange: true,
       keypadOptions: {
         accumulatorOptions: {
+          //REVIEW: Why is there documentation on this here?
           // {number} - maximum number of digits that can be entered on the keypad.x
           maxDigits: 8,
 
