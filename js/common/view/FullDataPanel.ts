8a0b882dd7bb0126f059744bc0e92ca1296a286c
phet-dev <phetadmin@gmail.com>
2023-02-03 04:34:56 -0700

    update copyright dates from daily grunt work

--- js/common/view/FullDataPanel.ts
+++ js/common/view/FullDataPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * The panel that shows the numeric values of Mass, Position and Velocity.
--- js/common/view/PathsWebGLNode.ts
+++ js/common/view/PathsWebGLNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * WebGL Script that renders the planets' paths.
