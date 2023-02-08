b4f2a279534880d26018c2956e8a845f033b3108
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-08 10:32:47 -0700

    Adding active to BodyInfo, so we maintain body color on restarts

--- js/intro/model/IntroModel.ts
+++ js/intro/model/IntroModel.ts
@@ -31,8 +31,8 @@ class IntroModel extends CommonModel<NumericalEngine> {
   public override reset(): void {
     super.reset();
     this.setInitialBodyStates( [
-      { mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -5 ) },
-      { mass: 10, position: new Vector2( 200, 0 ), velocity: new Vector2( 0, 100 ) }
+      { active: true, mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -5 ) },
+      { active: true, mass: 10, position: new Vector2( 200, 0 ), velocity: new Vector2( 0, 100 ) }
     ] );
   }
 }
