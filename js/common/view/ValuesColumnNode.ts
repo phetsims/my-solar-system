8ffaf4a29becc53e840a91c22bcf521f57ed91cf
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-03-24 16:28:58 -0500

    Leaving V at single decimal, keeping position at 2, see https://github.com/phetsims/my-solar-system/issues/133

--- js/common/view/ValuesColumnNode.ts
+++ js/common/view/ValuesColumnNode.ts
@@ -189,7 +189,7 @@ export default class ValuesColumnNode extends VBox {
         velocityRange,
         SolarSystemCommonStrings.units.kmsStringProperty,
         body.userControlledVelocityProperty,
-        body.colorProperty, model.isPlayingProperty, 2, {
+        body.colorProperty, model.isPlayingProperty, 1, {
           onEditCallback: clearPathsCallback
         }
       );
@@ -205,7 +205,7 @@ export default class ValuesColumnNode extends VBox {
         velocityRange,
         SolarSystemCommonStrings.units.kmsStringProperty,
         body.userControlledVelocityProperty,
-        body.colorProperty, model.isPlayingProperty, 2, {
+        body.colorProperty, model.isPlayingProperty, 1, {
           onEditCallback: clearPathsCallback
         }
       );
