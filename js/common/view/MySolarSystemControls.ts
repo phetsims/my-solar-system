5cad091d7ca9b1ea3b94bdce89c8188a4e5a18b1
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-08-24 12:10:49 -0500

    Adding the gravity scale slider, see https://github.com/phetsims/keplers-laws/issues/120

--- js/common/view/MySolarSystemControls.ts
+++ js/common/view/MySolarSystemControls.ts
@@ -6,20 +6,16 @@
  * @author Agustín Vallejo (PhET Interactive Simulations)
  */
 
-import { HBox, HSeparator, Node, RichText, Text, TextOptions, VBox, VBoxOptions } from '../../../../scenery/js/imports.js';
+import { HSeparator, Node, VBox, VBoxOptions } from '../../../../scenery/js/imports.js';
 import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
-import { combineOptions, EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import mySolarSystem from '../../mySolarSystem.js';
 import SolarSystemCommonConstants from '../../../../solar-system-common/js/SolarSystemCommonConstants.js';
 import createVisibilityInformationCheckboxes from '../../../../solar-system-common/js/view/createVisibilityInformationCheckboxes.js';
 import createArrowsVisibilityCheckboxes from '../../../../solar-system-common/js/view/createArrowsVisibilityCheckboxes.js';
 import createOrbitalInformationCheckboxes from './createOrbitalInformationCheckboxes.js';
 import MySolarSystemModel from '../model/MySolarSystemModel.js';
-import SolarSystemCommonColors from '../../../../solar-system-common/js/SolarSystemCommonColors.js';
-import SolarSystemCommonStrings from '../../../../solar-system-common/js/SolarSystemCommonStrings.js';
-import Dimension2 from '../../../../dot/js/Dimension2.js';
-import HSlider from '../../../../sun/js/HSlider.js';
-import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
+import GravityScaleSlider from '../../../../solar-system-common/js/view/GravityScaleSlider.js';
 
 type SelfOptions = EmptySelfOptions;
 
@@ -32,56 +28,12 @@ export default class MySolarSystemControls extends VBox {
     topLayer: Node,
     providedOptions: MySolarSystemControlsOptions
   ) {
-
-    const rangeMin = model.forceScaleProperty.range.min;
-    const rangeMax = model.forceScaleProperty.range.max;
-    const rangeStep = 2;
-
-    // This slider controles the zoom level of the vector arrows
-    const slider = new HSlider( model.forceScaleProperty, model.forceScaleProperty.range, {
-      trackSize: new Dimension2( 100, 4 ),
-      thumbSize: new Dimension2( 14, 28 ),
-      tickLabelSpacing: 3,
-      constrainValue: ( power: number ) => Math.abs( power ) < 0.5 ? 0 : power,
-      shiftKeyboardStep: 0.5,
-      keyboardStep: 1,
-      pageKeyboardStep: 2,
-      trackFillEnabled: SolarSystemCommonColors.foregroundProperty,
-      majorTickStroke: SolarSystemCommonColors.foregroundProperty,
-      majorTickLength: 8,
-      minorTickLength: 8,
-      minorTickStroke: SolarSystemCommonColors.foregroundProperty,
-
-      accessibleName: SolarSystemCommonStrings.a11y.scaleSliderStringProperty,
-
-      valueChangeSoundGeneratorOptions: {
-        numberOfMiddleThresholds: ( rangeMax - rangeMin ) / rangeStep - 1
-      }
-    } );
-
-    slider.addMajorTick( rangeMin, new RichText( MathSymbols.TIMES + `10<sup>${rangeMin}</sup`, SolarSystemCommonConstants.TEXT_OPTIONS ) );
-    slider.addMajorTick( rangeMax, new RichText( MathSymbols.TIMES + `10<sup>${rangeMax}</sup`, SolarSystemCommonConstants.TEXT_OPTIONS ) );
-
-    for ( let i = 0; i <= 6; i += 2 ) {
-      slider.addMinorTick( i );
-    }
-
     super( {
       children: [
         ...createOrbitalInformationCheckboxes( model, providedOptions.tandem ),
         new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
         ...createArrowsVisibilityCheckboxes( model, providedOptions.tandem ),
-        new HBox( {
-          spacing: 0,
-          enabledProperty: model.gravityVisibleProperty,
-          children: [
-            new Text( SolarSystemCommonStrings.zoomStringProperty, combineOptions<TextOptions>( {
-              maxWidth: SolarSystemCommonConstants.TEXT_MAX_WIDTH / 2,
-              layoutOptions: { leftMargin: 20 }
-            }, SolarSystemCommonConstants.TEXT_OPTIONS ) ),
-            slider
-          ]
-        } ),
+        new GravityScaleSlider( model.forceScaleProperty, model.gravityVisibleProperty ),
         new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
         ...createVisibilityInformationCheckboxes( model, providedOptions.tandem )
       ],
