1024dd066f7c3ffd8ed7e3f9a63062ac2c7f1562
pixelzoom <cmalley@pixelzoom.com>
2023-10-13 08:14:55 -0600

    rename modeMap to orbitalSystemMap, https://github.com/phetsims/my-solar-system/issues/200

--- js/lab/model/LabModel.ts
+++ js/lab/model/LabModel.ts
@@ -16,8 +16,11 @@ import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 
 export default class LabModel extends MySolarSystemModel {
 
+  // The OrbitalSystem that is currently selected
   public readonly orbitalSystemProperty: EnumerationProperty<OrbitalSystem>;
-  private readonly modeMap: Map<OrbitalSystem, BodyInfo[]>;
+
+  // Maps an OrbitalSystem to the BodyInfo[] that describes the configuration of Bodies in that system
+  private readonly orbitalSystemMap: Map<OrbitalSystem, BodyInfo[]>;
 
   public constructor( tandem: Tandem ) {
     super( {
@@ -47,7 +50,7 @@ export default class LabModel extends MySolarSystemModel {
       this.orbitalSystemProperty.value = OrbitalSystem.CUSTOM;
     } );
 
-    this.modeMap = new Map<OrbitalSystem, BodyInfo[]>();
+    this.orbitalSystemMap = new Map<OrbitalSystem, BodyInfo[]>();
     this.initializeModeMap();
 
     this.orbitalSystemProperty.link( mode => {
@@ -57,7 +60,7 @@ export default class LabModel extends MySolarSystemModel {
         this.userControlledProperty.value = false;
         this.isAnyBodyCollidedProperty.reset();
         this.timeProperty.reset();
-        const modeInfo = this.modeMap.get( mode );
+        const modeInfo = this.orbitalSystemMap.get( mode );
         this.loadBodyInfo( modeInfo! );
         this.numberOfActiveBodiesProperty.value = this.bodies.length;
         this.followCenterOfMass();
@@ -97,70 +100,70 @@ export default class LabModel extends MySolarSystemModel {
   }
 
   /**
-   * Initializes the keys and values for this.modeMap.
+   * Initializes the keys and values for this.orbitalSystemMap.
    */
   private initializeModeMap(): void {
-    this.modeMap.set( OrbitalSystem.SUN_PLANET, [
+    this.orbitalSystemMap.set( OrbitalSystem.SUN_PLANET, [
       { isActive: true, mass: 250, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -11.1 ) },
       { isActive: true, mass: 25, position: new Vector2( 200, 0 ), velocity: new Vector2( 0, 111 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.SUN_PLANET_MOON, [
+    this.orbitalSystemMap.set( OrbitalSystem.SUN_PLANET_MOON, [
       { isActive: true, mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, 0 ) },
       { isActive: true, mass: 10, position: new Vector2( 160, 0 ), velocity: new Vector2( 0, 120 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 140, 0 ), velocity: new Vector2( 0, 53 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.SUN_PLANET_COMET, [
+    this.orbitalSystemMap.set( OrbitalSystem.SUN_PLANET_COMET, [
       { isActive: true, mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, 0 ) },
       { isActive: true, mass: 1, position: new Vector2( 150, 0 ), velocity: new Vector2( 0, 120 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -220, 130 ), velocity: new Vector2( -20, -35 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.TROJAN_ASTEROIDS, [
+    this.orbitalSystemMap.set( OrbitalSystem.TROJAN_ASTEROIDS, [
       { isActive: true, mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, 0 ) },
       { isActive: true, mass: 5, position: new Vector2( 150, 0 ), velocity: new Vector2( 0, 119 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 75, -130 ), velocity: new Vector2( 103, 60 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 75, 130 ), velocity: new Vector2( -103, 60 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.ELLIPSES, [
+    this.orbitalSystemMap.set( OrbitalSystem.ELLIPSES, [
       { isActive: true, mass: 250, position: new Vector2( -200, 0 ), velocity: new Vector2( 0, 0 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -115, 0 ), velocity: new Vector2( 0, 151 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 50, 0 ), velocity: new Vector2( 0, 60 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 220, 0 ), velocity: new Vector2( 0, 37 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.HYPERBOLIC, [
+    this.orbitalSystemMap.set( OrbitalSystem.HYPERBOLIC, [
       { isActive: true, mass: 250, position: new Vector2( 0, 25 ), velocity: new Vector2( 0, 0 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -250, -70 ), velocity: new Vector2( 120, 0 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -250, -140 ), velocity: new Vector2( 120, 0 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -250, -210 ), velocity: new Vector2( 120, 0 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.SLINGSHOT, [
+    this.orbitalSystemMap.set( OrbitalSystem.SLINGSHOT, [
       { isActive: true, mass: 200, position: new Vector2( 1, 0 ), velocity: new Vector2( 0, -1 ) },
       { isActive: true, mass: 10, position: new Vector2( 131, 55 ), velocity: new Vector2( -55, 115 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -6, -128 ), velocity: new Vector2( 83, 0 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.DOUBLE_SLINGSHOT, [
+    this.orbitalSystemMap.set( OrbitalSystem.DOUBLE_SLINGSHOT, [
       { isActive: true, mass: 200, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -1 ) },
       { isActive: true, mass: 5, position: new Vector2( 0, -112 ), velocity: new Vector2( 134, 0 ) },
       { isActive: true, mass: 5, position: new Vector2( 186, -5 ), velocity: new Vector2( 1, 111 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( 70, 72 ), velocity: new Vector2( -47, 63 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.BINARY_STAR_PLANET, [
+    this.orbitalSystemMap.set( OrbitalSystem.BINARY_STAR_PLANET, [
       { isActive: true, mass: 150, position: new Vector2( -100, 0 ), velocity: new Vector2( 0, -60 ) },
       { isActive: true, mass: 120, position: new Vector2( 100, 0 ), velocity: new Vector2( 0, 50 ) },
       { isActive: true, mass: 0.000001, position: new Vector2( -50, 0 ), velocity: new Vector2( 0, 120 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.FOUR_STAR_BALLET, [
+    this.orbitalSystemMap.set( OrbitalSystem.FOUR_STAR_BALLET, [
       { isActive: true, mass: 120, position: new Vector2( -100, 100 ), velocity: new Vector2( -50, -50 ) },
       { isActive: true, mass: 120, position: new Vector2( 100, 100 ), velocity: new Vector2( -50, 50 ) },
       { isActive: true, mass: 120, position: new Vector2( 100, -100 ), velocity: new Vector2( 50, 50 ) },
       { isActive: true, mass: 120, position: new Vector2( -100, -100 ), velocity: new Vector2( 50, -50 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.DOUBLE_DOUBLE, [
+    this.orbitalSystemMap.set( OrbitalSystem.DOUBLE_DOUBLE, [
       { isActive: true, mass: 60, position: new Vector2( -115, -3 ), velocity: new Vector2( 0, -154 ) },
       { isActive: true, mass: 70, position: new Vector2( 102, 0 ), velocity: new Vector2( 1, 150 ) },
       { isActive: true, mass: 55, position: new Vector2( -77, -2 ), velocity: new Vector2( -1, 42 ) },
       { isActive: true, mass: 62, position: new Vector2( 135, 0 ), velocity: new Vector2( -1, -52 ) }
     ] );
-    this.modeMap.set( OrbitalSystem.CUSTOM, [
+    this.orbitalSystemMap.set( OrbitalSystem.CUSTOM, [
       { isActive: true, mass: 120, position: new Vector2( -100, 100 ), velocity: new Vector2( -50, -50 ) },
       { isActive: true, mass: 120, position: new Vector2( 100, 100 ), velocity: new Vector2( -50, 50 ) },
       { isActive: true, mass: 120, position: new Vector2( 100, -100 ), velocity: new Vector2( 50, 50 ) },
