11930073e9627eb379b7e33985b455dddcad7130
pixelzoom <cmalley@pixelzoom.com>
2023-10-16 13:22:17 -0600

    instrument startingBodyInfoProperty, convert BodyInfo from type to class, create BodyInfoIO, https://github.com/phetsims/my-solar-system/issues/226

--- js/intro/model/IntroModel.ts
+++ js/intro/model/IntroModel.ts
@@ -12,6 +12,7 @@ import mySolarSystem from '../../mySolarSystem.js';
 import MySolarSystemModel from '../../common/model/MySolarSystemModel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
+import BodyInfo from '../../../../solar-system-common/js/model/BodyInfo.js';
 
 
 export default class IntroModel extends MySolarSystemModel {
@@ -20,8 +21,8 @@ export default class IntroModel extends MySolarSystemModel {
     super( {
       // MySolarSystemModelOptions
       defaultBodyInfo: [
-        { isActive: true, mass: 250, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -11.1 ) },
-        { isActive: true, mass: 25, position: new Vector2( 200, 0 ), velocity: new Vector2( 0, 111 ) }
+        new BodyInfo( { isActive: true, mass: 250, position: new Vector2( 0, 0 ), velocity: new Vector2( 0, -11.1 ) } ),
+        new BodyInfo( { isActive: true, mass: 25, position: new Vector2( 200, 0 ), velocity: new Vector2( 0, 111 ) } )
       ],
       tandem: tandem
     } );
