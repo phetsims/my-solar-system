9c57338a5dc44d8ad3e0dbba67aaaf535a41a31a
pixelzoom <cmalley@pixelzoom.com>
2023-10-26 19:27:39 -0600

    fix range to allow for collided bodies, https://github.com/phetsims/my-solar-system/issues/237

--- js/lab/model/LabModel.ts
+++ js/lab/model/LabModel.ts
@@ -31,7 +31,7 @@ export default class LabModel extends MySolarSystemModel {
         new BodyInfo( { isActive: false, mass: 0.1, position: new Vector2( 1.00, 0 ), velocity: new Vector2( 0, 31.6834 ) } ),
         new BodyInfo( { isActive: false, mass: 0.1, position: new Vector2( -1.00, -1.00 ), velocity: new Vector2( 25.3467, 0 ) } )
       ],
-      numberOfActiveBodiesIsVariable: true,
+      numberOfBodiesIsVariable: true,
       isLab: true,
       tandem: tandem
     } );
