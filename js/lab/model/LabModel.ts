9836c6cb39e6a6af56781b286f498ef42617609e
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-12-12 14:07:35 -0500

    Making gravityForceScalePowerProperty not readOnly, see https://github.com/phetsims/keplers-laws/issues/220
    (cherry picked from commit d71d9c612be08d3ab69a040b782cb9aaf4efc3b4)
    (cherry picked from commit e01789e2a3f141ad6fa3bc7bd11b27ad6f99e53e)

--- js/lab/model/LabModel.ts
+++ js/lab/model/LabModel.ts
@@ -15,6 +15,7 @@ import Tandem from '../../../../tandem/js/Tandem.js';
 import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 import BodyInfo from '../../../../solar-system-common/js/model/BodyInfo.js';
 import MySolarSystemColors from '../../common/MySolarSystemColors.js';
+import isSettingPhetioStateProperty from '../../../../tandem/js/isSettingPhetioStateProperty.js';
 
 export default class LabModel extends MySolarSystemModel {
 
@@ -87,7 +88,7 @@ export default class LabModel extends MySolarSystemModel {
         this.gravityForceScalePowerProperty.reset();
 
         // Adjusting the gravity force scale in this system to prevent it from looking like an offensive symbol
-        if ( orbitalSystem === OrbitalSystem.FOUR_STAR_BALLET ) {
+        if ( orbitalSystem === OrbitalSystem.FOUR_STAR_BALLET && !isSettingPhetioStateProperty.value ) {
           this.gravityForceScalePowerProperty.value = -1.1;
         }
       }
