cdf85ff07fa546917aaa46fb5c5d60e55121e9ca
phet-dev <phetadmin@gmail.com>
2024-03-23 03:23:34 -0600

    update copyright dates from daily grunt work

--- js/common/view/ValuesColumnNode.ts
+++ js/common/view/ValuesColumnNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Generates the information column for values depending on the type of Value used.
--- js/common/view/VisibilityControlPanel.ts
+++ js/common/view/VisibilityControlPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * MySolarSystemControls is the main control panel, containing several checkboxes and a slider.
