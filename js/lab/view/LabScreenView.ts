7fd67e0c08d078c34c8afb66b425b7c8aaefd8a7
phet-dev <phetadmin@gmail.com>
2024-10-28 03:50:22 -0600

    update copyright dates from daily grunt work

--- js/lab/view/LabScreenView.ts
+++ js/lab/view/LabScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Screen View for Lab Screen: Where you can play with all the presets and body configurations
