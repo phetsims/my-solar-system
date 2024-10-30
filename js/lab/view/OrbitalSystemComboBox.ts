176b9987dcbe9f095e9a6e2db5947d89fcab6dc3
phet-dev <phetadmin@gmail.com>
2024-10-30 03:18:04 -0600

    update copyright dates from daily grunt work

--- js/lab/view/OrbitalSystemComboBox.ts
+++ js/lab/view/OrbitalSystemComboBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * OrbitalSystemComboBox is the control for selecting an OrbitalSystem (configuration of bodies).
