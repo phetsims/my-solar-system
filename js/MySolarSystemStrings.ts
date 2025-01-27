3dfd7157ff3a2a1ef7905b1cdedc887d64539b8b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-27 12:39:33 -0700

    add "supportsDynamicLocale": true to sims that support it but didn't have it, https://github.com/phetsims/joist/issues/1005

--- js/MySolarSystemStrings.ts
+++ js/MySolarSystemStrings.ts
@@ -13,128 +13,78 @@ import mySolarSystem from './mySolarSystem.js';
 
 type StringsType = {
   'my-solar-system': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
   'screen': {
-    'intro': string;
     'introStringProperty': LocalizedStringProperty;
-    'lab': string;
     'labStringProperty': LocalizedStringProperty;
   };
-  'centerOfMass': string;
   'centerOfMassStringProperty': LocalizedStringProperty;
-  'followCenterOfMass': string;
   'followCenterOfMassStringProperty': LocalizedStringProperty;
-  'returnBodies': string;
   'returnBodiesStringProperty': LocalizedStringProperty;
   'dataPanel': {
-    'X': string;
     'XStringProperty': LocalizedStringProperty;
-    'Y': string;
     'YStringProperty': LocalizedStringProperty;
-    'Vx': string;
     'VxStringProperty': LocalizedStringProperty;
-    'Vy': string;
     'VyStringProperty': LocalizedStringProperty;
-    'mass': string;
     'massStringProperty': LocalizedStringProperty;
-    'velocity': string;
     'velocityStringProperty': LocalizedStringProperty;
-    'position': string;
     'positionStringProperty': LocalizedStringProperty;
-    'bodies': string;
     'bodiesStringProperty': LocalizedStringProperty;
-    'moreData': string;
     'moreDataStringProperty': LocalizedStringProperty;
   };
-  'mass': string;
   'massStringProperty': LocalizedStringProperty;
   'mode': {
-    'sunAndPlanet': string;
     'sunAndPlanetStringProperty': LocalizedStringProperty;
-    'sunPlanetAndMoon': string;
     'sunPlanetAndMoonStringProperty': LocalizedStringProperty;
-    'sunPlanetAndComet': string;
     'sunPlanetAndCometStringProperty': LocalizedStringProperty;
-    'trojanAsteroids': string;
     'trojanAsteroidsStringProperty': LocalizedStringProperty;
-    'ellipses': string;
     'ellipsesStringProperty': LocalizedStringProperty;
-    'hyperbolic': string;
     'hyperbolicStringProperty': LocalizedStringProperty;
-    'slingshot': string;
     'slingshotStringProperty': LocalizedStringProperty;
-    'doubleSlingshot': string;
     'doubleSlingshotStringProperty': LocalizedStringProperty;
-    'binaryStarPlanet': string;
     'binaryStarPlanetStringProperty': LocalizedStringProperty;
-    'fourStarBallet': string;
     'fourStarBalletStringProperty': LocalizedStringProperty;
-    'doubleDouble': string;
     'doubleDoubleStringProperty': LocalizedStringProperty;
-    'custom': string;
     'customStringProperty': LocalizedStringProperty;
-    'orbitalSystem1': string;
     'orbitalSystem1StringProperty': LocalizedStringProperty;
-    'orbitalSystem2': string;
     'orbitalSystem2StringProperty': LocalizedStringProperty;
-    'orbitalSystem3': string;
     'orbitalSystem3StringProperty': LocalizedStringProperty;
-    'orbitalSystem4': string;
     'orbitalSystem4StringProperty': LocalizedStringProperty;
   };
   'units': {
-    'kg': string;
     'kgStringProperty': LocalizedStringProperty;
   };
   'unitsInfo': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
-    'content': string;
     'contentStringProperty': LocalizedStringProperty;
-    'content2': string;
     'content2StringProperty': LocalizedStringProperty;
-    'content3': string;
     'content3StringProperty': LocalizedStringProperty;
   };
   'pattern': {
-    'labelParenthesesUnits': string;
     'labelParenthesesUnitsStringProperty': LocalizedStringProperty;
-    'range': string;
     'rangeStringProperty': LocalizedStringProperty;
   };
   'keyboardHelpDialog': {
-    'chooseAnOrbitalSystem': string;
     'chooseAnOrbitalSystemStringProperty': LocalizedStringProperty;
-    'orbitalSystems': string;
     'orbitalSystemsStringProperty': LocalizedStringProperty;
-    'orbitalSystem': string;
     'orbitalSystemStringProperty': LocalizedStringProperty;
   };
   'a11y': {
-    'moreData': string;
     'moreDataStringProperty': LocalizedStringProperty;
-    'info': string;
     'infoStringProperty': LocalizedStringProperty;
-    'numberOfBodies': string;
     'numberOfBodiesStringProperty': LocalizedStringProperty;
     'introScreen': {
       'screenSummary': {
-        'playAreaDescription': string;
         'playAreaDescriptionStringProperty': LocalizedStringProperty;
-        'controlAreaDescription': string;
         'controlAreaDescriptionStringProperty': LocalizedStringProperty;
       }
     };
     'labScreen': {
       'screenSummary': {
-        'playAreaDescription': string;
         'playAreaDescriptionStringProperty': LocalizedStringProperty;
-        'controlAreaDescription': string;
         'controlAreaDescriptionStringProperty': LocalizedStringProperty;
       };
-      'orbitalSystemSelector': string;
       'orbitalSystemSelectorStringProperty': LocalizedStringProperty;
     }
   }
